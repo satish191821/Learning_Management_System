e9ed806cc05d15e3ce859a7e79f0aecbee57529b
satish sahu <satishworld191821@gmail.com>
2025-10-02 16:36:16 +0530

    solve errors in token.js,inndex.js

--- backend/config/token.js
+++ backend/config/token.js
@@ -6,8 +6,9 @@ const genToken = async (userId) => {
     const token = await jwt.sign({ userId }, process.env.JWT_SECRET, {
       expiresIn: "7d",
     });
-    console.log(token);
+
     return token;
+    
   } catch (error) {
     console.log(error);
   }
--- backend/index.js
+++ backend/index.js
@@ -4,14 +4,22 @@ import connectDb from './config/connectDB.js'
 import cookieParser from 'cookie-parser'
 import authRouter from './route/authRoute.js'
 dotenv.config()
+import cors from "cors"
+
 
 // create express app 
 const port= process.env.PORT
 const app=express()
 app.use(express.json())// middleware
 app.use(cookieParser())// middleware
+//cors package for conneccting frontend
+app.use(cors ({
+    origin:"http://localhost:5175",
+    credentials:true
+}))
 
-app.use("/api/auth",authRouter)// for auth. checking 
+
+app.use("/api/auth",authRouter)// api for auth. checking 
 
 app.get("/",(req,res)=>{
     res.send("Hello from server")
